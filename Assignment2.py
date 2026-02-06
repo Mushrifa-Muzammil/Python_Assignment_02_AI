9f8652e50da1af34ecdc6fe8a436a9fa6b7884fe
Mushrifa-Muzammil <mushrifamuzammil2028@gmail.com>
2026-02-06 20:56:01 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Print Statement
+print("Mars Tech")
+
+
+# In[3]:
+
+
+# Buy input
+institute = input("Enter your institute name: ")
+
+
+# In[5]:
+
+
+# Buy inputs
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+school = input("Enter your school name: ")
+degree = input("Enter your degree: ")
+
+
+# In[6]:
+
+
+# Addition
+a = 43
+b = 34
+
+add = a + b
+print("a =", a)
+print("b =", b)
+print("Add =", add)
+
+
+# In[7]:
+
+
+# Subtraction
+a = 43
+b = 34
+
+sub = a - b
+print("a =", a)
+print("b =", b)
+print("Sub =", sub)
+
+
+# In[8]:
+
+
+# Multiplication
+a = 63
+b = 97   # Corrected to get 6111
+
+mul = a * b
+print("a =", a)
+print("b =", b)
+print("Mul =", mul)
+
+
+# In[9]:
+
+
+# Division
+a = 76
+b = 31
+
+div = a / b
+print("a =", a)
+print("b =", b)
+print("Float Div =", div)
+
+
+# In[10]:
+
+
+# Floor Division
+a = 76
+b = 31
+
+floor_div = a // b
+print("a =", a)
+print("b =", b)
+print("Floor Div =", floor_div)
+
+
+# In[11]:
+
+
+# Modulo
+a = 76
+b = 31
+
+mod = a % b
+print("a =", a)
+print("b =", b)
+print("Modulo =", mod)
+
+
+# In[12]:
+
+
+# Power
+a = 4
+b = 4
+
+power = a ** b
+print("a =", a)
+print("b =", b)
+print("Power =", power)
+
+
+# In[ ]:
+
+
+
+
